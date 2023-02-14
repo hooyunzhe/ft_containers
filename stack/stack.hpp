79bcfae0e86ae41e5ffdc124e4311b9b2ed3e2d8
Hoo Yun Zhe <yunzhescientist@gmail.com>
2023-02-14 16:59:33 +0800

    Added stack, which uses ft:vector as the underlying container

---
+++ stack/stack.hpp
@@ -0,0 +1,40 @@
+#ifndef STACK_HPP
+# define STACK_HPP
+
+# include "../vector/vector.hpp"
+
+namespace	ft
+{
+	template <class T, class Container = ft::vector<T> >
+	class	stack {
+		public:
+			typedef Container					container_type;
+			typedef Container::value_type		value_type;
+			typedef Container::size_type		size_type;
+			typedef Container::reference		reference;
+			typedef Container::const_reference	const_reference;
+
+			explicit stack(const container_type &container = container_type());
+			~stack();
+			stack	&operator = (const stack &stack_var);
+
+			reference	top(void);
+			const_reference	top(void) const;
+			container_type	data(void);
+			const container_type	data(void) const;
+
+			bool	empty(void) const;
+			size_type	size(void) const;
+
+			void	push(const_reference value);
+			void	pop(void);
+			void	swap(stack &stack_var);
+
+		private:
+			container_type	_container;
+	};
+}
+
+# include "stack.tpp"
+
+#endif
